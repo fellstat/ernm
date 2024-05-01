ccd744db1019837fa9c9d37166e7f6176c9ea77d
duncan-clark <duncan-clark@hotmail.co.uk>
2024-05-01 16:36:05 -0400

    cleaned up"

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Estimation of fully/partially observed Exponential-Family Random     Network Mod
 
 ## Adding Statistics
 
-Adding statistics directly into the ERNM package required the following steps:
+The preferred method for users to use their own custom statistics is to create a package as in the example extension package in inst/examplePackage. Users can then directly call register*Statistic.
+
+For Statistics that may be useful for the wider community. They can be integrated into the ERNM package as follows. This workflow also forces users to adequately test their statistics, which may be beneficial, especially for complex statistics.
 1. Add to the inst/include/stats.h file. This is where the calculation of the statistics defined. Note for ERNM the dyadUpdate and discreteVertexUpdate and continVertexUpdate methods are crucial for the performance of the MCMC routine required to fit ERNMs.
 2. Register the statistic with the src/statController.cpp file
 3. Add the statistic to the test_stat.cpp file. This will then be run when the package is built to ensure the statistic is safe on the C++ end 
--- tests/testthat/test-stats.R
+++ tests/testthat/test-stats.R
@@ -18,7 +18,7 @@ test_that("Stats", {
         return(net)
     }
     
-    make_net = function(...){
+    make_net <- function(...){
         tmp <- matrix(rnorm(10000)>2,nrow = 100)
         net <- as.network(tmp,directed =F)
         set.vertex.attribute(net,"var_1",as.character(apply(rmultinom(100,1,rep(1/3,3)),2,FUN = function(x){which(x==1)})))
@@ -31,20 +31,20 @@ test_that("Stats", {
         net <- add_treated_neighs(net,"var_3")
     }
     
-    net = make_net()
-    nets = lapply(1:100,FUN = make_net)
+    net<-make_net()
+    nets<-lapply(1:100,FUN = make_net)
     
     
     # ================
     # Hamming Distance
     # ================
     
-    edges = as.edgelist(net)*1
-    edges = cbind(as.double(edges[,1]),as.double(edges[,2]))
-    edges = cbind(edges[,1],edges[,2])
+    edges <- as.edgelist(net)*1
+    edges <- cbind(as.double(edges[,1]),as.double(edges[,2]))
+    edges <- cbind(edges[,1],edges[,2])
     head(edges)
     
-    hamming_calc = function(edges,net){
+    hamming_calc <- function(edges,net){
         
         e_list = as.edgelist(net)*1.0
         e_list = cbind(as.double(e_list[,1]),as.double(e_list[,2]))
@@ -60,107 +60,107 @@ test_that("Stats", {
         ernm::calculateStatistics(net ~ hamming(edges,100)) == hamming_calc(edges,net)
     )
     
-    edges[1,] = c(1,11)
+    edges[1,] <-  c(1,11)
     hamming_test_2 <- (
         ernm::calculateStatistics(net ~ hamming(edges,100)) == hamming_calc(edges,net)
     )
     
-    edges[2,] = c(1,13)
+    edges[2,] <-  c(1,13)
     hamming_test_3 <- (
         ernm::calculateStatistics(net ~ hamming(edges,100)) == hamming_calc(edges,net)
     )
     
     
     # check dyad update function
-    edges = as.edgelist(net)*1
-    edges = cbind(as.double(edges[,1]),as.double(edges[,2]))
+    edges <- as.edgelist(net)*1
+    edges <- cbind(as.double(edges[,1]),as.double(edges[,2]))
     
     # update included edge that network has
-    model = ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
-    model = model$m$sampler$getModel()
+    model <- ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
+    model <- model$m$sampler$getModel()
     model$setNetwork(ernm::as.BinaryNet(net))
     model$calculate()
     
     hamming_calc(edges,net)
-    net_tmp = net
+    net_tmp <- net
     delete.edges(net_tmp,get.edgeIDs(net_tmp,1,61))
-    r_stat_1 = hamming_calc(edges,net_tmp)
+    r_stat_1 <- hamming_calc(edges,net_tmp)
     
     model$statistics()
     model$dyadUpdate(1,61)
-    cpp_stat_1 =  model$statistics()
+    cpp_stat_1 <- model$statistics()
     
     # update included edge that network has
-    model = ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
-    model = model$m$sampler$getModel()
+    model <- ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
+    model <- model$m$sampler$getModel()
     model$setNetwork(ernm::as.BinaryNet(net))
     model$calculate()
     
     hamming_calc(edges,net)
-    net_tmp = net
+    net_tmp <- net
     delete.edges(net_tmp,get.edgeIDs(net_tmp,61,1))
-    r_stat_2 = hamming_calc(edges,net_tmp)
+    r_stat_2 <- hamming_calc(edges,net_tmp)
     
     model$statistics()
     model$dyadUpdate(61,1)
-    cpp_stat_2 =  model$statistics()
+    cpp_stat_2 <- model$statistics()
     
     # update included edge that network does not have
-    edges = matrix(c(1,2),nrow = 1)
-    model = ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
-    model = model$m$sampler$getModel()
+    edges <- matrix(c(1,2),nrow = 1)
+    model <- ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
+    model <- model$m$sampler$getModel()
     model$setNetwork(ernm::as.BinaryNet(net))
     model$calculate()
     model$statistics()
     
     hamming_calc(edges,net)
-    net_tmp = net
+    net_tmp <- net
     add.edges(net_tmp,1,2)
-    r_stat_3 = hamming_calc(edges,net_tmp)
+    r_stat_3 <- hamming_calc(edges,net_tmp)
     
     model$statistics()
     model$dyadUpdate(1,2)
-    cpp_stat_3 = model$statistics()
+    cpp_stat_3 <- model$statistics()
     
     # update not included edge that network has
-    edges = matrix(c(1,2),nrow = 1)
-    model = ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
-    model = model$m$sampler$getModel()
+    edges <- matrix(c(1,2),nrow = 1)
+    model <- ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
+    model <- model$m$sampler$getModel()
     model$setNetwork(ernm::as.BinaryNet(net))
     
     hamming_calc(edges,net)
-    net_tmp = net
+    net_tmp <- net
     delete.edges(net_tmp,get.edgeIDs(net_tmp,1,61))
-    r_stat_4 = hamming_calc(edges,net_tmp)
+    r_stat_4 <- hamming_calc(edges,net_tmp)
     
     model$statistics()
     model$dyadUpdate(1,61)
-    cpp_stat_4 = model$statistics()
+    cpp_stat_4 <- model$statistics()
     
     # update not included edge that network does not have
-    edges = as.edgelist(net)*1.0
-    edges = cbind(as.double(edges[,1]),as.double(edges[,2]))
+    edges <- as.edgelist(net)*1.0
+    edges <- cbind(as.double(edges[,1]),as.double(edges[,2]))
     head(edges)
     
-    model = ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
-    model = model$m$sampler$getModel()
+    model <- ernm(net ~ hamming(edges,100),maxIter = 2,mcmcBurnIn = 100,mcmcInterval = 10, mcmcSampleSize = 100)
+    model <- model$m$sampler$getModel()
     model$setNetwork(ernm::as.BinaryNet(net))
     model$calculate()
 
     hamming_calc(edges,net)
-    net_tmp = net
+    net_tmp <- net
     add.edges(net_tmp,1,2)
-    r_stat_5 = hamming_calc(edges,net_tmp)
+    r_stat_5 <- hamming_calc(edges,net_tmp)
     
     model$statistics()
     model$dyadUpdate(1,2)
-    cpp_stat_5 = model$statistics()
+    cpp_stat_5 <- model$statistics()
     
-    hamming_test_4 = (r_stat_1 == cpp_stat_1)
-    hamming_test_5 = (r_stat_2 == cpp_stat_2)
-    hamming_test_6 = (r_stat_3 == cpp_stat_3)
-    hamming_test_7 = (r_stat_4 == cpp_stat_4)
-    hamming_test_7 = (r_stat_5 == cpp_stat_5)
+    hamming_test_4 <- (r_stat_1 == cpp_stat_1)
+    hamming_test_5 <- (r_stat_2 == cpp_stat_2)
+    hamming_test_6 <- (r_stat_3 == cpp_stat_3)
+    hamming_test_7 <- (r_stat_4 == cpp_stat_4)
+    hamming_test_7 <- (r_stat_5 == cpp_stat_5)
 
     testthat::expect_true(hamming_test_1)
     testthat::expect_true(hamming_test_2)
