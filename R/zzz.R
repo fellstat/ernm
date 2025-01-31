05ef3e6282308b9e354b5da6496f1ade99a0c698
duncan-clark <duncan-clark@hotmail.co.uk>
2025-01-31 10:51:13 -0500

    reran with roxygenize

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,16 @@
+#' @useDynLib ernm, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @import BH
+#' @import trust
+#' @import moments
 loadModule("ernm",TRUE)
 .onLoad <- function(libname, pkgname){
-	.Call("_ernm_initStats")
-	.Call("_ernm_initToggles")
+    # Skip initialization when running devtools::document()
+    if (nzchar(Sys.getenv("ROXYGEN_PKG"))) {
+        return()
+    }
+    .Call("_ernm_initStats")
+    .Call("_ernm_initToggles")
 }
 .onUnload <- function(libpath) {}
